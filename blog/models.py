08124e411941fd69c705562d3d6ae2817fbf9598
Jason Dolan <jasondolan@Peters-MBP-2.home>
2020-02-05 16:10:43 +0200

    polished

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,16 @@ class Blog(models.Model):
 	body = models.TextField()
 	image = models.ImageField(upload_to='images/')
 
+
+	def summary(self):
+		return self.body[:100]
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
+
+	def __str__(self):
+		return self.title
+
 # Add the blog app to the settings
 
 # Create a migration
